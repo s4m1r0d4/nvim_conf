0bb681ddb388b3f8b08b7b00b84d39a85e324f84
rc <reddragon987pro@gmail.com>
2025-07-02 10:48:50 -0600

    feat(LSP): configure DartLs

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -226,6 +226,28 @@ return {
                             capabilities = require('cmp_nvim_lsp').default_capabilities(),
                             cmd = { "jdtls" },
                         })
+                    end,
+                    dartls = function ()
+                        require("lspconfig").dartls.setup({
+                            cmd = { "dart", "language-server", "--protocol=lsp" },
+                            filetypes = { "dart" },
+                            init_options = {
+                                closingLabels = true,
+                                flutterOutline = true,
+                                onlyAnalyzeProjectsWithOpenFiles = true,
+                                outline = true,
+                                suggestFromUnimportedLibraries = true,
+                            },
+                            -- root_dir = root_pattern("pubspec.yaml"),
+                            settings = {
+                                dart = {
+                                    completeFunctionCalls = true,
+                                    showTodos = true,
+                                },
+                            },
+                            on_attach = function(client, bufnr)
+                            end,
+                        })
                     end
                 }
             })
